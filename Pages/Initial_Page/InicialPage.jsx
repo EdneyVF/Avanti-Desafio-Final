300d7bb5818f1ca6fe5a6c05ed9cf4dbc83116f3
drakkar95 <raonidutracunha@ymail.com>
2023-12-08 20:51:47 -0300

    Adicionado arquivo pagina inicial

---
+++ Pages/Initial_Page/InicialPage.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function InicialPage() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+      <h1>Pagina Inicial</h1>
+      </div>
+     
+      
+    </>
+  )
+}
+
+export default InicialPage
